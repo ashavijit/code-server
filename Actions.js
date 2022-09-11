cd84f6a0dc9619610c1ff57923340f2d5a84c96b
ashavijit <as2440@it.jgec.ac.in>
2022-09-11 19:16:56 +0530

    okk

---
+++ Actions.js
@@ -0,0 +1,10 @@
+const ACTIONS = {
+    JOIN: "join",
+    JOINED: "joined",
+    CODE_CHANGE: "code_change",
+    SYNC_CODE: "sync_code",
+    DISCONNECTED: "disconnected",
+    LEAVE: "leave",
+};
+
+module.exports = ACTIONS;
